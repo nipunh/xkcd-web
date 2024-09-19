2adc45710e5ed0a74d92664e14711bc77b24418b
nipunh <nipunhedaoo@gmail.com>
2024-09-19 14:22:01 -0400

    ui improvements

--- server.js
+++ server.js
@@ -13,37 +13,37 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // CORS setup for local dev
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 // Routes
 app.get('/api/latest', async (req, res) => {
-  try {
-    const response = await axios.get('https://xkcd.com/info.0.json');
-    res.json(response.data);
-  } catch (err) {
-    res.status(500).json({ message: 'Error fetching latest comic' });
-  }
+    try {
+        const response = await axios.get('https://xkcd.com/info.0.json');
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching latest comic' });
+    }
 });
 
 app.get('/api/comic/:id', async (req, res) => {
-  try {
-    const comicId = req.params.id;
-    const response = await axios.get(`https://xkcd.com/${comicId}/info.0.json`);
-    viewCounts[comicId] = (viewCounts[comicId] || 0) + 1;
-    res.json({...response.data, viewCount: viewCounts[comicId]}); 
-  } catch (err) {
-    res.status(500).json({ message: 'Error fetching comic' });
-  }
+    try {
+        const comicId = req.params.id;
+        const response = await axios.get(`https://xkcd.com/${comicId}/info.0.json`);
+        viewCounts[comicId] = (viewCounts[comicId] || 0) + 1;
+        res.json({ ...response.data, viewCount: viewCounts[comicId] });
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching comic' });
+    }
 });
 
 // Serve React app
-app.get('*', (req, res) => { 
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
